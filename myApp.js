dfa26b36c37e9d4fc674a53a5a0543c59f84bd60
Anders Skibsted <abo@internettet.email>
2024-07-14 12:51:14 +0000

    second commit

--- myApp.js
+++ myApp.js
@@ -1,10 +1,30 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+require('dotenv').config();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
+app.use('/public', express.static(__dirname + '/public'));
 
-
-
+app.get('/json', (req, res) => {
+    //let mess = 'Hello json';
+    console.log(process.env);
+    if (process.env.MESSAGE_STYLE==="uppercase") {
+        //const mess = 'Hello json'.toUpperCase();
+        res.json({'message': 'Hello json'.toUpperCase()});
+    }
+    else {
+        console.log(process.env.MESSAGE_STYLE);
+        console.log('Hello json'.toUpperCase());
+        //const mess = 'Hello json'.toLowerCase();
+        res.json({'message': 'Hello json'});
+    }
+    //const obj = {'message': mess};
+    
+})
 
 
 
